7d803f9a02f5032fe0b82e9538c71bfa5fef63ce
Ondřej Vondra <ondrej.vondra@live.com>
2019-08-14 15:55:59 +0200

    oob login without cache

--- loginOOB.R
+++ loginOOB.R
@@ -10,4 +10,4 @@ client_app <- oauth_app('sessionsExample', key = 'publicApiNativeClient')
 
 client_token <- oauth2.0_token(mluvii_endpoint, client_app,
                                scope = "mluviiPublicApi",
-                               use_oob = TRUE)
+                               use_oob = TRUE, cache = FALSE)
